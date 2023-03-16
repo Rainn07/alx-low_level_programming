af0ae6eb2cee52ff14bf38355d6fdb504d0ac871
Rainn07 <jmzimela7@gmail.com>
2023-03-16 23:27:01 +0000

    discription of script added, betty happy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'printing without the use of print'
+ * @parameter: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
