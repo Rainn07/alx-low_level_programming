2501460772dd48f886a2bbd635db3dbef443bad2
Rainn07 <jmzimela7@gmail.com>
2023-03-19 22:25:30 +0000

    task 0 betty compliant

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-/*
-* func creates a number and then prints out if the number is 
-* positive or negative
+/**
+ * main - Entry point of the program
+ *
+ * description - func creates a number and then prints out if the number is
+ * positive or negative
+ *
+ * Return: Always 0
 */
 int main(void)
 {
