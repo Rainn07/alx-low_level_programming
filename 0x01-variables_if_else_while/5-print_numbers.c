04f4d989e4cfea5b95a80e9778901da40faa0964
Rainn07 <jmzimela7@gmail.com>
2023-03-20 02:03:59 +0000

    task five. print single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * description: printing the single digits
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int single_num;
+	for (single_num = 0; single_num <= 9; single_num++)
+	{
+		printf("%d", single_num);
+	}
+	return (0);
+}
