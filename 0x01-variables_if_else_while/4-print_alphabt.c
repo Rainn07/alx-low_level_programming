9bfacc97ec9d20bed24e4ea45730119b17baf873
Rainn07 <jmzimela7@gmail.com>
2023-03-20 01:52:09 +0000

    task 4, print all but e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point to the program
+ *
+ * Description - variable lcase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lcase;
+
+	for (lcase = 'a'; lcase <= 'z'; lcase++)
+	{
+		if (lcase != 'e' && lcase != 'q')
+		{
+			putchar(lcase);
+		}
+	}
+	putchar('\n');
+}
