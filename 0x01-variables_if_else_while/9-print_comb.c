4d8539615af1c3db93c2a2cb9d619d2aaeb2fc3f
Rainn07 <jmzimela7@gmail.com>
2023-03-20 08:20:22 +0000

    task 9, betty ccompliant

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point to the program
+ *
+ * description - Printing numbers separated by a comma
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x + '0');
+		if ((x + '0') < (9 + '0'))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+	putchar('\n');
+	return (0);
