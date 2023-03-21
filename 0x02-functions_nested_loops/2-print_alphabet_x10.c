3bea366327966e2eef4aaded369258ac8deb4f33
Rainn07 <jmzimela7@gmail.com>
2023-03-21 20:23:32 +0000

    task 1 complete, betty compliant

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times then an empty line
+ *
+ * no parameters
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+
+	while (i < 9)
+	{
+		for (char j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		i++;
+	}
+}
+
