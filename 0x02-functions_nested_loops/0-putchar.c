570aee498383e62c503aeec63da18826d541f5e4
Rainn07 <jmzimela7@gmail.com>
2023-03-21 17:19:46 +0000

    add the files to the repository. first draft of func

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <main.h>
+/**
+ * Printer - prints the word "_putchar"
+ *
+ * no parameters
+ *
+ * Return: Void (Success)
+ */
+void Printer(void)
+{
+	printf("%s \n", "_putchar");
+}
