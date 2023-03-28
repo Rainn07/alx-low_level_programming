056012675d0a9afa4f85b8ce1a7c22707d39cce8
Rainn07 <jmzimela7@gmail.com>
2023-03-28 02:33:24 +0000

    task completed using std lib function: printf

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_array - function that prints out elements
  * of array
@@ -14,12 +15,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar(a[i] + '0');
+		printf("%d", a[i]);
 
 		if (i < n - 1)
 		{
-			_putchar(',');
-			_putchar(32);
+			printf(", ");
 		}
 	}
 	_putchar('\n');
