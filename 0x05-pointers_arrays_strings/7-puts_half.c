b5fb09331b820a00fe717cd5a58de6678a94bcc1
Rainn07 <jmzimela7@gmail.com>
2023-03-28 02:05:44 +0000

    task 7. printing second half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * puts_half - function that prints the second half of a string
+ *
+ * @str: the string from which characters will be printed
+ *
+ * Return: Void
+ */
+void puts_half(char *str)
+{
+	int i;
+	int length = 0;
+
+	/*loop to find length of str*/
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	/*what to do if length is even/odd*/
+	if (length % 2 == 0)
+	{
+		length = length / 2;
+	}
+	else
+	{
+		length  = (length + 1) / 2;
+	}
+	while (str[length] != '\0')
+	{
+		length++;
+		_putchar(str[length];
+	}
+	_putchar('\n');
+}
