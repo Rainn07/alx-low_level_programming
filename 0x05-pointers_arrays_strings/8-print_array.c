16287984cdd1e653a1fca1994fbbcb36a9e0649c
Rainn07 <jmzimela7@gmail.com>
2023-03-28 02:28:45 +0000

    task 8 complete.

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,10 +14,13 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", a[i]);
+		_putchar(a[i] + '0');
 
 		if (i < n - 1)
-			printf(", ");
+		{
+			_putchar(',');
+			_putchar(32);
+		}
 	}
 	_putchar('\n');
 }
