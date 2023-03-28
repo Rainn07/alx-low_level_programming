fff1925bc847e71482f8036dae280eb9c72c9754
Rainn07 <jmzimela7@gmail.com>
2023-03-28 00:58:24 +0000

    printing str in reverse. uses another function within it: _strlen

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - funct that prints a str in reverse
+ *
+ * @s: the string to be printed in reverse
+ *
+ * Return: Void
+ */
+void print_rev(char *s)
+{
+	int i = _strlen(&s);
+
+	while (i != 0)
+	{
+		i--;
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
