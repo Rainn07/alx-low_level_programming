ee508a9a03b47d992a5b8c53196ed8bb47dba81d
Rainn07 <jmzimela7@gmail.com>
2023-03-28 00:31:11 +0000

    task 0, betty style compliant

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reset_to_89 - funct that takes a pointer to int and changes the value to 98
+ * reset_to_98 - funct that takes a pointer to int and changes the value to 98
  *
  * @n: parameter, a pointer to an int
  *
