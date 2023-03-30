07c15d9f935fc28c238f623e2438c7f8c21bd7e6
Rainn07 <jmzimela7@gmail.com>
2023-03-30 21:03:21 +0000

    task 0-betty compliant

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,27 @@
 #include <string.h>
 #include "main.h"
-
+/**
+ * _strcat - function that joins one string to another
+ * @dest: the string
+ * @src: string to be added
+ *
+ * Return: ptr to concatenated
+ */
 char *_strcat(char *dest, char *src)
 {
-	char *ptr =dest; /*ptr to the pointer to dest*/
+	char *ptr = dest;
+	/*ptr to the pointer to dest*/
 
 	while (*ptr)
 	{
 		ptr++;
-	}/*moving up to the last character of dest*/
+		/*moving up to the last character of dest*/
+	}
 	while (*src)
 	{
 		*ptr++ = *src++;
-	}/*next char in *ptr is the next char in *src*/
+		/*next char in *ptr is the next char in *src*/
+	}
 	*ptr = '\0';
-	return dest;
+	return (dest);
 }
