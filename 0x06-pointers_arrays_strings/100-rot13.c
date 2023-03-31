38caadb16a0a5e82f26a06d0a32b9e4ed1708fe1
Rainn07 <jmzimela7@gmail.com>
2023-03-31 13:28:22 +0000

    task 8, advanced task

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rot13 - function that encodes
+ * a string by shifting the alphabets
+ * by 13 and wraps around
+ *
+ * @str: str to be encoded
+ *
+ * Return: encoded str.
+ */
+char *rot13(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (isalpha(str[i]))
+		{
+			if (islower(str[i]))
+				str[i] = (((str[i] - 'a') + 13) % 26) + 'a';
+			else
+				str[i] = (((str[i] - 'A') + 13) % 26) + 'A';
+		}
+	}
+	return (str);
+}
