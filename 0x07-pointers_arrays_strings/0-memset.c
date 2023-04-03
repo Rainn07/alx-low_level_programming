d730923ac0d141217b07b44ae2a0baa6b14bb3e1
Rainn07 <jmzimela7@gmail.com>
2023-04-03 20:34:55 +0000

    task 0. setting memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fills first n bytes of memeory
+ * area pointed to by s with constant byte
+ *
+ * @s: Pointer to memory to be filled
+ * @b: char to fill memory
+ * @n: number of bytes to be filled
+ *
+ * Return: Void
+ */
+char *_memset(void *s, int b, unsigned int n)
+{
+	unsigned int index;
+
+	for (index = 0; index < n; index++)
+		s[index] = b;
+
+	return (s);
+}
+
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,2 @@
+int i_puchar(char c);
+char * _memset(char *s, char b, unsigned int n);
