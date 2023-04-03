aed8d66f305335a6f0cca7cacdbdda9d6950a767
Rainn07 <jmzimela7@gmail.com>
2023-04-03 22:45:54 +0000

    task 6 complete

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_chessboard - prints chessboard
+ *
+ * @a: rows of the array
+ *
+ * Return: Void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int row, col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			if (col == 7)
+			{
+				_putchar(a[row][col]);
+				_putchar('\n');
+			}
+			else
+				_putchar(a[row][col]);
+		}
+	}
+}
+
+
