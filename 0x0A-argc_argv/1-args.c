fee0112f9f8ad777a0b698d4a4dd1c22b2928f1b
Rainn07 <jmzimela7@gmail.com>
2023-04-16 21:12:52 +0000

    print number og command line args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - funct that prints number of arguments
+ * @argc: number of arguments
+ * @argv: vector array of pointers
+ *
+ * Return: 0 success.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)*argv;
+	printf("%d\n", argc);
+		return (0);
+}
