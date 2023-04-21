22634b82436fd6e5de3fff55bd6f06dfd58cee05
Rainn07 <jmzimela7@gmail.com>
2023-04-21 14:30:09 +0000

    task 3 late submission

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "stdio.h"
+#include <stdlib.h>
+/** main - prints the product of arguments
+ * @args: number of arguments
+ * @argv: vector array of pointers
+ *
+ * Return: 0 Success
+ * 	   1 for failure
+ */
+int main(int args, char *argv[])
+{
+	int num1, num2;
+	
+	if (args != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	printf("%d\n", num1 * num2);
+
+	return (0);
+
+}
