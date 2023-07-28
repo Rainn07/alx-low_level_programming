58ee75c5ae8305d35a81cbbd77d5901f14f400c8
Rainn07 <jmzimela7@gmail.com>
2023-07-28 11:16:31 +0000

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all elements in linked list
+ * @h: linked list to be printed
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	/* number of list */
+       size_t number_of_nodes = 0;
+
+       /* empty list condition */
+       if (h == NULL)
+       {
+	       printf("[0] (nil)\n");
+	       return number_of_nodes;
+       }
+
+/*iterate through list and print*/
+       const list_t* current = h;
+       while (current != NULL)
+       {
+	       printf(%d, current->data);
+	       current = current->next;
+	       number_of_nodes++;
+       }
+       printf("\n");
+       return (number_of_nodes);
+}
