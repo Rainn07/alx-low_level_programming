d24cf12cf0a0555f31ddc64db654430d81b2412f
Rainn07 <jmzimela7@gmail.com>
2023-07-31 09:39:16 +0000

    header file included

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,8 +1,6 @@
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 #include "lists.h"
-#include <stddef.h>
+
 /**
  * print_list - prints all elements in linked list
  * @h: pointer to linked list to be printed
@@ -15,16 +13,13 @@ size_t print_list(const list_t *h)
 /* number of elements in list */
 	size_t number_of_nodes = 0;
 
-/* empty list condition */
+
 	while (h)
 	{
-	if (h->str == NULL)
+	if (!h->str)
 		printf("[0] (nil)\n");
-		return (number_of_nodes);
-
 	else
-		printf("[%u] %s\n", h->len, h->str);
-
+		printf("%s [%u]\n", h->str, h->len);
 
 	h = h->next;
 	number_of_nodes++;
