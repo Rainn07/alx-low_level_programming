87913582b6192dd70979b883563ff26bca8498bb
Rainn07 <jmzimela7@gmail.com>
2023-08-16 11:39:56 +0000

    all used functions defined

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,6 +2,23 @@
 #include <stdio.h>
 
 /**
+ * _strlen_recursion - returns length of a string
+ * @s: str for which we calucate the length
+ *
+ * Return: len of str int
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+	return (len);
+}
+/**
  * is_palin - checks is start char equal end char
  * @s: char point to str
  * @start: start char
