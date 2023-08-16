1dd344e5e6e72f9859b84ecb096615b92364d825
Rainn07 <jmzimela7@gmail.com>
2023-08-16 11:21:21 +0000

    main.h prototypes

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,3 +11,5 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_numer(int n);
+int is_palindrome(char *s);
+int is_palin(char *s, int start, int end);
