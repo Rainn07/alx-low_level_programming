8a6e177147a4a1d6a339cda991adfa123db9960b
Rainn07 <jmzimela7@gmail.com>
2023-08-30 11:31:38 +0000

    task 8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/** len_ignore_wild - length of str.
+ * @str: str to be counted
+ * 
+ * Return length of str
+ */
+int len_ignore_wild(char *str)
+{
+	int len = 0;
+	int count = 0;
+
+	if (*(
+
+
+/**
+ * wildcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 1 for identical str
+ */
+
