702d4c19790f9e98f38534cb329b0fde8daaad66
Rainn07 <jmzimela7@gmail.com>
2023-09-06 09:21:23 +0000

    task 3, multiply args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+/*
+ * Author: Jaxn
+ * task: 3
+ * Project: 0x0A-argc_argv
+ */
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiple two arguments
+ * @argc: number of arguments
+ * @argv: vector of ptrs to arguments
+ *
+ * Return: void. always zero
+ */
+
+int main(int argc, char *argv[])
+{
+	int arg1, arg2, product;
+
+	if (argc != 3)
+		printf("Error");
+	return (1);
+
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[2]);
+	product = arg1 * arg2;
+
+	printf("%d\n", product);
+
+	return (0);
+}
