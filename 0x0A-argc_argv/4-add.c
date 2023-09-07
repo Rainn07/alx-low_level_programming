7d4c08a87a45b4826f5acc034af546dee27a4865
Rainn07 <jmzimela7@gmail.com>
2023-09-07 08:16:04 +0000

    add two ints

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+/*
+ * Author: Jaxn
+ * task 4: 4-add.c
+ * Project: 0x0A-argc_argv
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+/**
+ * main - adds two positive numbers
+ * @argc: number of arguments
+ * @argv: vector of ptrs to arguments
+ *
+ * Return: void. always zero
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	/*if no number is passed*/
+	if (argc < 2)
+	{
+		printf("0\n");
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			/* if non digit is found */
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
