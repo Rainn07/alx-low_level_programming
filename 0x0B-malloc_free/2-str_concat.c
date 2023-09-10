6dabb6c0fe14749e815e465acc2ef5d435d30618
Rainn07 <jmzimela7@gmail.com>
2023-09-10 09:13:07 +0000

    task 2: str concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+/*
+ * Author: Jaxn
+ * task 1: 1_strdup.c
+ * Project: 0x0B-malloc_free
+ */
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _str_concat - return ptr to 2 concatenated str
+ * @s1: first string
+ * @s2: second string
+ *
+ *
+ * Return: NULL if str == NULL
+ *	return ptr to copied str - success.
+ */
+char *_str_concat(char *s1, char *s2)
+{
+	int counter, concat_counter = 0, len = 0;
+	char *placeholder;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+
+	for (counter = 0; s1[counter] || s2[counter]; counter++)
+		len++;
+
+	placeholder = (char *)malloc((len + 1) * sizeof(char));
+
+	if (placeholder == NULL)
+		return (NULL);
+
+	for (counter = 0; s1[counter]; counter++)
+		placeholder[concat_counter++] = s1[counter];
+
+	for (counter = 0; s2[counter]; counter++)
+		placeholder[concat_counter++] = s2[counter];
+
+	return (placeholder);
+}
