81ca46966629911ec8fda952ea37f89f51832f52
Rainn07 <jmzimela7@gmail.com>
2023-09-11 20:45:57 +0000

    task 1 and 2

---
+++ 0x0B-malloc_free/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef MYCONSTANTS_H
+#define MYCONSTANTS_H
+/*
+ * 0-object_like_macro.h
+ * header file that defines macro
+ * names SIZE as an abbreviation
+ * for the token 1024
+ */
+
+#define SIZE 1024
+
+#endif
