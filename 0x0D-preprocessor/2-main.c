820db9ccc649f85d9394d6415ecae95b9c00a368
Rainn07 <jmzimela7@gmail.com>
2023-09-11 20:57:37 +0000

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints name of profram src file
+ *
+ * Return: 0
+ */
+
+int main() {
+	printf("The name of the current source file is: %s\n", __FILE__);
+	return 0;
+}
