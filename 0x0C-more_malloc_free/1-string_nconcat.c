ea3bfea4db5c32414523918088200121eeb2bb5e
Rainn07 <jmzimela7@gmail.com>
2023-09-20 14:19:22 +0000

    task 1 betty compliant

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -40,14 +40,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	unsigned int i, j;
 
-	if (s1 == NULL) s1 = "";
-	if (s2 == NULL) s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	if (n >= _strlen(s2)) n = _strlen(s2);
+	if (n >= _strlen(s2))
+		n = _strlen(s2);
 
-	ptr = (char *)malloc( _strlen(s1) + n + 1);
+	ptr = (char *)malloc(_strlen(s1) + n + 1);
 
-	if (ptr == NULL) return (NULL);
+	if (ptr == NULL)
+		return (NULL);
 
 	for (i = 0; s1[i] != '\0'; i++)
 		ptr[i] = s1[i];
@@ -55,7 +59,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (j = 0; j < n; j++)
 		ptr[i + j] = s2[j];
 
-	ptr[i+ j] = '\0';
+	ptr[i + j] = '\0';
 
 	return (ptr);
 }
