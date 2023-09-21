d6b36dc46d4a49079c90e1e92eb068473889fb12
Rainn07 <jmzimela7@gmail.com>
2023-09-21 09:00:37 +0000

    initial commit with task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/*
+ * Auth: Jaxn
+ * Project: 0x12 - singly linked lists
+ * task 0: 0-print_list.c
+ * 21/09/2023
+ */
+
+/**
+ * print_list - prints elements of list
+ * @h: singly linked list
+ *
+ * Return: number of elements in list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t counter;
+
+	counter = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] (nil)\n", h->len);
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}
