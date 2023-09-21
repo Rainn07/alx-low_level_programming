3ede3f7415b669667303ee03089b1e9e647ffa56
Rainn07 <jmzimela7@gmail.com>
2023-09-21 10:23:53 +0000

    task 0: null str output

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -22,7 +22,7 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[%d] (nil)\n", h->len);
+			printf("[%d] (nil)\n",0);
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
