efc11eb210f84e063a0f60284805cdfd541e8c59
Rainn07 <jmzimela7@gmail.com>
2023-09-25 06:28:18 +0000

    task 0: headerfile

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog types
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Desc: define dog type
+ */
+
+typedef struct dog
+{
+        char *name;
+        float age;
+        char *owner;
+}
+
+#endif 
