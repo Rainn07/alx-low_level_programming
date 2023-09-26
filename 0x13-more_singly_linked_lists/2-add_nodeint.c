b10d4d4928a77b7f368274e6d6afaa6e312aefd4
Rainn07 <jmzimela7@gmail.com>
2023-09-26 13:52:14 +0000

    task 2 & header update

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdlib.h>
+/*
+ * Auth: Jaxn
+ * Project: 0x13 - more singly linked lists
+ * task 2: 2-add_nodent.c
+ * 26/09/2023
+ */
+
+/**
+ * add_node - adds node at the beginning of list_t list
+ * @head: ptr to start of existing list
+ * @str: str to be copied into new node
+ *
+ * Return: address of new head element
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_head;
+
+	new_head = (listint_t *)malloc(sizeof(listint_t));
+	if (new_head == NULL)
+		exit(98);
+
+	new_head->n = n;
+
+	new_head->next = *head;
+	*head = new_head;
+
+	return (new_head);
+}
