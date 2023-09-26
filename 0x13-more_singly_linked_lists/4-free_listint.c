92d913cba118b416126d6acdee5efc0838ccfb99
Rainn07 <jmzimela7@gmail.com>
2023-09-26 14:13:58 +0000

    task 4 and updated header

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+/*
+ * Auth: Jaxn
+ * Project: 0x13 - more singly linked lists
+ * task 4: 4-free_listint.c
+ * 21/09/2023
+ */
+
+/**
+ * free_listint - frees a list_t list
+ * @head: pointer to start of list
+ *
+ * Return: Void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *iterator;
+
+	while (head)
+	{
+		iterator = head->next;
+		free(head);
+		free(iterator);
+	}
+}
