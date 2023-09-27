c0654f1a48db85a7d6330f1a28f96733e049d64e
Rainn07 <jmzimela7@gmail.com>
2023-09-27 21:15:17 +0000

    task 2, syntax coorection

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,21 +12,22 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d == 0)
+	{
 		return;
-
-	if (d->name == NULL)
-		printf("Name: (nil)\n");
+	}
 	else
-		printf("Name: %s\n", d->name);
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
 
-	if (d->age < 0)
-		printf("Age: -1.0\n");
-	else
 		printf("Age: %f\n", d->age);
-	
-	if (d->owner == NULL)
-		printf("Owner: (nil)\n");
-	else
-		printf("Owner: %s\n", d->owner);
+
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
 }
