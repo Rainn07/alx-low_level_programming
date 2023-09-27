7dec302f0e8ce96d6fd21e5e4640c954e12802ee
Rainn07 <jmzimela7@gmail.com>
2023-09-27 18:53:35 +0000

    task 03

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,7 +16,10 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
-typdef struct dog dog_t;
+/**
+ * dog_t - type def
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
