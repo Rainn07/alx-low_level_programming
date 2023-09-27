35465f5c01483f85bbb584fb2c304ac4c6942a89
Rainn07 <jmzimela7@gmail.com>
2023-09-27 18:43:30 +0000

    task 2 complete

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/*
+ * Auth: Jaxn
+ * Project: 0x0E -  Structures and typedef
+ */
+
+/**
+ * print_dog - Print struct
+ * @d: struct to be printed
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	if (d->age < 0)
+		printf("Age: invalid\n");
+	else
+		printf("Age: %f\n", d->age);
+
+	if(d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
+}
