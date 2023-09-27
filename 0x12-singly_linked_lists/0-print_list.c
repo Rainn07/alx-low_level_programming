117aec2bf0c0cf9a8609967e5d457e1d51688f86
Rainn07 <jmzimela7@gmail.com>
2023-09-27 21:23:58 +0000

    task 0. printf syntax

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -22,7 +22,7 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[%d] (nil)\n", 0);
+			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
