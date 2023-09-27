adc9aa5b2809f62ac3eb078c9df16d1e5c788cad
Rainn07 <jmzimela7@gmail.com>
2023-09-27 18:49:41 +0000

    task 2 betty

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -21,7 +21,7 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", d->name);
 
 	if (d->age < 0)
-		printf("Age: invalid\n");
+		printf("Age: (nil)\n");
 	else
 		printf("Age: %f\n", d->age);
 	
