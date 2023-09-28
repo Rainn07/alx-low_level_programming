9c31f1dc44f5f0e5db705711ed0aacc3d1bf2a22
Rainn07 <jmzimela7@gmail.com>
2023-09-28 13:31:15 +0000

    task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,31 @@
+/*
+ * Auth: Jaxn
+ * Project: 0x14. C - Bit manipulation
+ * Task: 5 counts number of bits to flip
+ */
+#include <stdio.h>
+#include "main.h"
+/**
+ * flip_bits - counts number of bits to
+ *		flip to make two number identical
+ * @n: first bit
+ * @m: second bit
+ *
+ * Return: -1 - error
+ *      number of bits to flip - Success
+ */
+int flip_bits(unsigned long int *n, unsigned int *m)
+{
+	unsigned int flip_count;
+	unsigned int xor_result;
+
+	xor_result = n ^ m;
+	xor_result = 0;
+
+	while (xor_result > 0)
+	{
+		flip_count += xor_result & 1;
+		xor_result >>= 1;
+	}
+	return (1);
+}
