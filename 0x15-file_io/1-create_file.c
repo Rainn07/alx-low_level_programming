e9e226d7268f11d92719442bf50012bc5a0faee3
Rainn07 <jmzimela7@gmail.com>
2023-10-05 20:31:25 +0000

    betty compliant code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];len++)
+		for (len = 0; text_content[len]; len++)
 			;
 		/*counting num of char in text*/
 	}
@@ -35,7 +35,7 @@ int create_file(const char *filename, char *text_content)
 	w = write(o, text_content, len);
 	if (w == -1)
 	{
-		close (o);
+		close(o);
 		return (-1);
 	}
 
